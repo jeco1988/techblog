198eb01af04c429710f098228f54d52ab5490fa4
Jack <jackeconomos@outlook.com>
2022-12-22 20:39:14 +1030

    updated routes + created HB files

---
+++ controllers/api/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const userRoutes = require('./userroutes.js');
+const postRoutes = require('./postroutes.js');
+const commentRoutes = require('./commentroutes.js');
+
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes)
+
+module.exports = router;
